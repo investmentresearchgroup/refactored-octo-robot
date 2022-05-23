01a183af487683b020507772f5578e943494b734
dswiredu <dswiredu@gmail.com>
2022-05-23 13:00:05 -0400

    First user template view setup

--- portfolio-tracker-project/portfoliotracker/singleclient/views.py
+++ portfolio-tracker-project/portfoliotracker/singleclient/views.py
@@ -3,7 +3,7 @@ from django.contrib import messages
 
 # Create your views here.
 def index(request):
-    messages.success(request, "Good Morning David!!")
-    messages.warning(request, "So so Morning David!!")
-    messages.error(request, "Bad Morning David!!")
+    # messages.success(request, "Good Morning David!!")
+    # messages.warning(request, "So so Morning David!!")
+    # messages.error(request, "Bad Morning David!!")
     return render(request,"singleclient/index.html")
